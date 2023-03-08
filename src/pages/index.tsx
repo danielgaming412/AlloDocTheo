f0329f40552b48f461b679158a90291ed0511bef
Hassan Helbawi <hassanhelbawi@macbook-pro-de-hassan.home>
2023-03-08 17:59:40 +0100

    feat: poser les bases technologiques du site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,13 @@
+import Image from 'next/image';
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
+import NextImage from '@/components/NextImage';
 import Seo from '@/components/Seo';
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Vercel from '~/svg/Vercel.svg';
+import BigRoundLogo from '../../public/images/BigRoundLogo.png';
+import HeaderHero from '../../public/images/large-og.png';
 
 export default function HomePage() {
   return (
@@ -24,42 +17,29 @@ export default function HomePage() {
 
       <main>
         <section className='bg-white'>
+          <div className='h-[100vh] w-full'>
+            <Image src={HeaderHero} alt='Allo Doc Theo' />
+          </div>
+
           <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-            <Vercel className='text-5xl' />
-            <h1 className='mt-4'>
-              Next.js + Tailwind CSS + TypeScript Starter
-            </h1>
+            <NextImage
+              src={BigRoundLogo}
+              width='180'
+              height='180'
+              alt='Allo Doc Theo'
+            />
+            <h1 className='mt-4'>Docteur Théodore</h1>
             <p className='mt-2 text-sm text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{' '}
+              Médecin généraliste spécialisé en nutrition anti-inflammatoire,
+              j'aide mes patients à adopter un mode de vie sain et équilibré. À
+              travers ce site, je souhaite vous transmettre tout mon savoir et
+              vous guider pour adopter les bases pour maîtriser votre santé !
             </p>
-            <p className='mt-2 text-sm text-gray-700'>
-              <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-                See the repository
-              </ArrowLink>
-            </p>
-
-            <ButtonLink className='mt-6' href='/components' variant='light'>
-              See all components
-            </ButtonLink>
-
-            <UnstyledLink
-              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-              className='mt-4'
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                width='92'
-                height='32'
-                src='https://vercel.com/button'
-                alt='Deploy with Vercel'
-              />
-            </UnstyledLink>
 
             <footer className='absolute bottom-2 text-gray-700'>
-              © {new Date().getFullYear()} By{' '}
+              © {new Date().getFullYear()} Par{' '}
               <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-                Theodorus Clarence
+                Docteur Théodore
               </UnderlineLink>
             </footer>
           </div>
